bf00f30bcb8bdfe485dd69bac372e7ffce5b68d4
Unknown <adriannetubig@gmail.com>
2017-10-22 08:57:55 +0800

    Refactor
    Refactored Business Logic.
    MVC Project is currently broken.

---
+++ AndersonCRMModel/Company.cs
@@ -0,0 +1,15 @@
+﻿using BaseModel;
+using System.Collections.Generic;
+
+namespace AndersonCRMModel
+{
+    public class Company : Base
+    {
+        public int CompanyId { get; set; }
+        
+        public string Color { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Employee> Employees { get; set; }
+    }
+}
---
+++ AndersonCRMModel/Peripheral.cs
@@ -0,0 +1,23 @@
+﻿using BaseModel;
+using System.Collections.Generic;
+
+namespace AndersonCRMModel
+{
+    public class Peripheral : Base
+    {
+        public int EmployeeId { get; set; }
+        public int PeripheralId { get; set; }
+        public int PeripheralTypeId { get; set; }
+        
+        public string AssetTag { get; set; }
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public string SerialNumber { get; set; }
+
+        public virtual Employee Employee { get; set; }
+        public virtual PeripheralType PeripheralType { get; set; }
+
+        public virtual ICollection<PeripheralHistory> PeripheralHistories { get; set; }
+    }
+
+}
